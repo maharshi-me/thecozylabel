065eae27a2c6967cd41acd8e63f82263509a13c4
maharshi-b <maharshi3781@gmail.com>
2020-10-04 13:48:38 +0530

    added s3 as static storage

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -54,6 +54,7 @@ INSTALLED_APPS = [
     'allauth',
     'allauth.account',
     'django.contrib.sites',
+    'storages',
     
 ]
 SITE_ID = 1
@@ -167,20 +168,28 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statics')
+AWS_ACCESS_KEY_ID = 'AKIA6EK5AWOEVHLA2VON'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'thecozylabel'
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
 
 STATICFILES_DIRS = [
-     os.path.join(BASE_DIR, "static")   
- ]
-
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AUTH_USER_MODEL = 'users.CustomUser'
 
 
 # Media settings
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
